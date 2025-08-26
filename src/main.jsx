14a96a5bbc8937ba31fed4a99fe69cb2222d4e9c
RejwaN <55855042+iamrejwan@users.noreply.github.com>
2025-08-27 03:43:11 +0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import RejwanPortfolio from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RejwanPortfolio />)
